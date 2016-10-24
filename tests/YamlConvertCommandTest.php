c3d5b2406e7c5490f5a69f2da30cca34bbde30fb
Martin Hasoň <martin.hason@gmail.com>
2016-10-24 12:37:39 +0200

    Simplify tests

--- tests/YamlConvertCommandTest.php
+++ tests/YamlConvertCommandTest.php
@@ -20,7 +20,7 @@ use Webuni\ComposerYamlPlugin\YamlConvertCommand;
 final class YamlConvertCommandTest extends \PHPUnit_Framework_TestCase
 {
     private $app;
-    private $output;
+    private $buffer;
     private $cwd;
 
     protected function setUp()
@@ -28,52 +28,49 @@ final class YamlConvertCommandTest extends \PHPUnit_Framework_TestCase
         $this->app = new Application();
         $this->app->setAutoExit(false);
         $this->app->add(new YamlConvertCommand());
-        $this->output = new BufferedOutput();
+        $this->buffer = new BufferedOutput();
 
         $this->cwd = getcwd();
         chdir(sys_get_temp_dir());
-        @unlink('composer.json');
-        @unlink('composer.yml');
+        $this->removeFiles();
     }
 
     protected function tearDown()
     {
-        chdir(sys_get_temp_dir());
-        @unlink('composer.json');
-        @unlink('composer.yml');
         chdir($this->cwd);
+        $this->removeFiles();
     }
 
     public function testDefaultInputFileDoesNotExists()
     {
-        $this->app->run(new ArrayInput(['yaml-convert']), $this->output);
-        $this->assertContains('The input file "composer.yml" does not exist.', $this->output->fetch());
+        $this->app->run(new ArrayInput(['yaml-convert']), $this->buffer);
+        $this->assertContains('The input file "composer.yml" does not exist.', $this->buffer->fetch());
     }
 
     public function testCustomInputFileDoesNotExists()
     {
-        $this->app->run(new ArrayInput(['yaml-convert', 'input' => 'composer.json']), $this->output);
-        $this->assertContains('The input file "composer.json" does not exist.', $this->output->fetch());
+        $this->app->run(new ArrayInput(['yaml-convert', 'input' => 'composer.json']), $this->buffer);
+        $this->assertContains('The input file "composer.json" does not exist.', $this->buffer->fetch());
     }
 
     public function testInvalidFormat()
     {
-        $this->app->run(new ArrayInput(['yaml-convert', 'input' => 'composer.json']), $this->output);
-        $this->assertContains('The input file "composer.json" does not exist.', $this->output->fetch());
+        $this->app->run(new ArrayInput(['yaml-convert', 'input' => 'composer.json']), $this->buffer);
+        $this->assertContains('The input file "composer.json" does not exist.', $this->buffer->fetch());
     }
 
     public function testSameFormat()
     {
         file_put_contents('composer.yml', 'name: package');
-        $this->app->run(new ArrayInput(['yaml-convert', 'output' => 'composer.yml']), $this->output);
-        $this->assertContains('Input format is same as output format.', $this->output->fetch());
+        $this->app->run(new ArrayInput(['yaml-convert', 'output' => 'composer.yml']), $this->buffer);
+        $this->assertContains('Input format is same as output format.', $this->buffer->fetch());
     }
 
     public function testConvertYamlToJson()
     {
         file_put_contents('composer.yml', 'name: package');
-        $this->app->run(new ArrayInput(['yaml-convert']), $this->output);
-        $this->assertContains('Converted "composer.yml" to "composer.json"', $this->output->fetch());
+        $this->app->run(new ArrayInput(['yaml-convert']), $this->buffer);
+        $this->assertContains('Converted "composer.yml" to "composer.json"', $this->buffer->fetch());
         $this->assertFileExists('composer.json');
         $this->assertEquals("{\n    \"name\": \"package\"\n}", file_get_contents('composer.json'));
     }
@@ -81,9 +78,19 @@ final class YamlConvertCommandTest extends \PHPUnit_Framework_TestCase
     public function testConvertJsonToYaml()
     {
         file_put_contents('composer.json', "{\n    \"name\": \"package\"\n}");
-        $this->app->run(new ArrayInput(['yaml-convert', 'input' => 'composer.json', 'output' => 'composer.yml']), $this->output);
-        $this->assertContains('Converted "composer.json" to "composer.yml"', $this->output->fetch());
+        $this->app->run(new ArrayInput(['yaml-convert', 'input' => 'composer.json', 'output' => 'composer.yml']), $this->buffer);
+        $this->assertContains('Converted "composer.json" to "composer.yml"', $this->buffer->fetch());
         $this->assertFileExists('composer.yml');
         $this->assertEquals("name: package\n", file_get_contents('composer.yml'));
+    }
+
+    private function removeFiles()
+    {
+        foreach (['json', 'yml'] as $extension) {
+            $file = sys_get_temp_dir().'/composer.'.$extension;
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
     }
 }
